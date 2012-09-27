b4cf165a3bcc41feded6237d12016acc28ab8baa
yongkangliu <yongkangliu@gmail.com>
2012-09-27 13:57:27 -0400

    ok

--- ITCS 6150 AI/src/PG1AStarAlg/AStarTest.java
+++ ITCS 6150 AI/src/PG1AStarAlg/AStarTest.java
@@ -26,7 +26,7 @@ public class AStarTest {
         assertEquals(true, result);
         assertEquals(612, TreeNode.getNumOfNodesExpanded());
         assertEquals(
-                "4 -> 5 -> 6 -> 1 -> 3 -> 2 -> 8 -> 3 -> 5 -> 4 -> 3 -> 5 -> 1 -> 6 -> 4 -> 1 -> 2 -> 7 -> 6 -> 4 -> 1 -> 2 -> Goal",
+                "move 4 -> move 5 -> move 6 -> move 1 -> move 3 -> move 2 -> move 8 -> move 3 -> move 5 -> move 4 -> move 3 -> move 5 -> move 1 -> move 6 -> move 4 -> move 1 -> move 2 -> move 7 -> move 6 -> move 4 -> move 1 -> move 2 -> Goal",
                 TreeNode.getMoveStepSequence());
         assertEquals(1109, TreeNode.getNumOfNodesGenerated());
         assertEquals(22, TreeNode.getNumOfStepsToGoal());
@@ -43,7 +43,8 @@ public class AStarTest {
 
         assertEquals(true, result);
         assertEquals(11, TreeNode.getNumOfNodesExpanded());
-        assertEquals("6 -> 8 -> 2 -> 1 -> 8 -> 6 -> 5 -> Goal", TreeNode.getMoveStepSequence());
+        assertEquals("move 6 -> move 8 -> move 2 -> move 1 -> move 8 -> move 6 -> move 5 -> Goal",
+                TreeNode.getMoveStepSequence());
         assertEquals(22, TreeNode.getNumOfNodesGenerated());
         assertEquals(7, TreeNode.getNumOfStepsToGoal());
     }
@@ -59,7 +60,8 @@ public class AStarTest {
 
         assertEquals(true, result);
         assertEquals(17, TreeNode.getNumOfNodesExpanded());
-        assertEquals("6 -> 5 -> 4 -> 8 -> 7 -> 4 -> 5 -> 6 -> Goal", TreeNode.getMoveStepSequence());
+        assertEquals("move 6 -> move 5 -> move 4 -> move 8 -> move 7 -> move 4 -> move 5 -> move 6 -> Goal",
+                TreeNode.getMoveStepSequence());
         assertEquals(33, TreeNode.getNumOfNodesGenerated());
         assertEquals(8, TreeNode.getNumOfStepsToGoal());
     }
@@ -76,7 +78,7 @@ public class AStarTest {
         assertEquals(true, result);
         assertEquals(5291, TreeNode.getNumOfNodesExpanded());
         assertEquals(
-                "5 -> 7 -> 2 -> 5 -> 3 -> 8 -> 7 -> 3 -> 6 -> 4 -> 5 -> 2 -> 3 -> 6 -> 4 -> 1 -> 8 -> 4 -> 1 -> 5 -> 2 -> 1 -> 4 -> 7 -> 6 -> 3 -> Goal",
+                "move 5 -> move 7 -> move 2 -> move 5 -> move 3 -> move 8 -> move 7 -> move 3 -> move 6 -> move 4 -> move 5 -> move 2 -> move 3 -> move 6 -> move 4 -> move 1 -> move 8 -> move 4 -> move 1 -> move 5 -> move 2 -> move 1 -> move 4 -> move 7 -> move 6 -> move 3 -> Goal",
                 TreeNode.getMoveStepSequence());
         assertEquals(8376, TreeNode.getNumOfNodesGenerated());
         assertEquals(26, TreeNode.getNumOfStepsToGoal());
